418c816e201c79dde660b3f4bc508efbb8ee3630
Imad Dabligi <imad.dabligi@gmail.com>
2024-11-15 15:32:50 +0100

    refactor(backend): ChatApp Done

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -43,31 +43,18 @@ class ApiConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-
-        cookies = self.scope.get("cookies")
-        token = cookies.get("access_token")
-
-        try:
-            decoded_token = AccessToken(token)
-            payload = decoded_token.payload
-            user_id = payload.get("user_id")
-            user = await sync_to_async(User.objects.filter(id=user_id).first)()
-
-            if user:
-                message = data.get("message")
-                sender = user.username
-                await self.channel_layer.group_send(
-                    self.room_group_name,
-                    {
-                        'type': 'chat_message', #Message Routing: broadcasts the message to all consumers instances in the specified group.
-                        'sender': sender,
-                        'message': message
-                    }
-                )
-        except Exception as e:
-            print("Error :", e)
-            pass
-
+        
+        message = data.get("message")
+        sender = data.get("sender")
+        # print("DATA : ", data)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message', #Message Routing: broadcasts the message to all consumers instances in the specified group.
+                'sender': sender,
+                'message': message
+            }
+        )
         # await self.send(text_data=json.dumps({
         #     'type': "Chat",
         #     'message': message
